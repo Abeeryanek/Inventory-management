fd7d8e15bc27f534547d5f6b2859608879722b6d
Abeeryanek <168862530+Abeeryanek@users.noreply.github.com>
2026-06-12 09:41:23 +0200

    feat:added services product and order

--- src/main/java/com/Abeer/store/service/OrderService.java
+++ src/main/java/com/Abeer/store/service/OrderService.java
@@ -1,8 +1,70 @@
 package com.abeer.store.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.abeer.store.dto.request.AddOrderItemRequest;
+import com.abeer.store.dto.request.CreateOrderRequest;
+import com.abeer.store.dto.response.OrderResponse;
+import com.abeer.store.entity.Order;
+import com.abeer.store.entity.OrderItem;
+import com.abeer.store.entity.Product;
+import com.abeer.store.entity.User;
+import com.abeer.store.exceptions.ResourceNotFoundException;
+import com.abeer.store.mapper.OrderMapper;
+import com.abeer.store.repository.OrderRepository;
+import com.abeer.store.repository.ProductRepository;
+import com.abeer.store.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
+@Transactional
 public class OrderService {
-    
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    @Transactional(readOnly=true)
+    public OrderResponse.Full findById(Long id){
+       Order order= findOrderOrThrow(id);
+       return orderMapper.toResponse(order);
+    }
+    @Transactional(readOnly=true)
+    public Page<OrderResponse.Full> findAll(Pageable pageable) {
+        return orderRepository.findAll(pageable).map(orderMapper::toResponse);
+    }
+    @Transactional
+    public OrderResponse.Full create(CreateOrderRequest request){
+        User user = userRepository.findById(request.userId())
+    .orElseThrow(()->new ResourceNotFoundException("User not found: " + request.userId()));
+        Order order = new Order(request.orderName(), user);
+        Order saved=orderRepository.save(order); 
+        return orderMapper.toResponse(saved);
+    }
+    @Transactional
+    public OrderResponse.Full addOrderItem(Long id,AddOrderItemRequest request){
+       Order order=findOrderOrThrow(id);
+       Product product = productRepository.findById(request.productId()).orElseThrow(()-> new ResourceNotFoundException("Product not found: " + request.productId()));
+       OrderItem orderItem= new OrderItem(order, product, product.getCurrentPrice(), request.quantity());
+       order.addOrderItem(orderItem);
+       Order saved = orderRepository.save(order);
+       return orderMapper.toResponse(saved);
+       
+    }
+    @Transactional
+    public void delete(Long id){
+        Order order=findOrderOrThrow(id);
+        order.cancel();
+        orderRepository.save(order);}
+
+    //private methods
+    private Order findOrderOrThrow(Long id){
+        Order order=orderRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Order not found: " + id));
+
+        return  order;
+    }
 }
--- src/main/java/com/Abeer/store/service/ProductService.java
+++ src/main/java/com/Abeer/store/service/ProductService.java
@@ -1,20 +1,66 @@
-// package com.abeer.store.service;
-// import org.springframework.stereotype.Service;
+package com.abeer.store.service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.abeer.store.dto.request.CreateProductRequest;
-// import com.abeer.store.dto.request.UpdatePriceRequest;
-// import com.abeer.store.dto.request.UpdateProductRequest;
-// import com.abeer.store.dto.response.ProductResponse;
-// import org.springframework.data.domain.Page; 
+import com.abeer.store.dto.request.CreateProductRequest;
+import com.abeer.store.dto.request.UpdatePriceRequest;
+import com.abeer.store.dto.request.UpdateProductRequest;
+import com.abeer.store.dto.response.ProductResponse;
+import com.abeer.store.entity.Product; 
+import com.abeer.store.exceptions.ResourceNotFoundException;
+import com.abeer.store.mapper.ProductMapper;
+import com.abeer.store.repository.ProductRepository;
 
-// import org.springframework.data.domain.Pageable; 
+import lombok.RequiredArgsConstructor;
 
-// @Service
-// public class ProductService {
-//       public ProductResponse.Full findById(Long id) { return null; }
-//     public Page<ProductResponse.Full> findAll(Pageable pageable) { return null; }
-//     public ProductResponse.Full create(CreateProductRequest request) { return null; }
-//     public ProductResponse.Full update(Long id, UpdateProductRequest request) { return null; }
-//     public ProductResponse.Full updatePrice(Long id, UpdatePriceRequest request) { return null; }
-//     public void delete(Long id) {}
-// }
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class ProductService {
+      private final ProductMapper productMapper;
+      private final ProductRepository productRepository;
+    @Transactional(readOnly=true)
+    public ProductResponse.Full findById(Long id) { 
+      Product product=findProductOrThrow(id);
+      return productMapper.toResponse(product); }
+
+    @Transactional(readOnly=true)
+    public Page<ProductResponse.Full> findAll(Pageable pageable) { 
+      return productRepository.findAll(pageable).map(productMapper::toResponse); }
+    @Transactional
+    public ProductResponse.Full create(CreateProductRequest request) { 
+      if(productRepository.existsByProductName(request.productName()))
+        throw new IllegalArgumentException(
+          "product with the name '"+request.productName() + "' alreday exists");
+      Product product= new Product(request.productName(),request.currentPrice(),request.stockQuantity());
+      Product saved = productRepository.save(product);
+      return productMapper.toResponse(saved);
+    }
+    @Transactional
+    public ProductResponse.Full update(Long id, UpdateProductRequest request) { 
+      Product product=findProductOrThrow(id);
+      product.updateDetails(request.productName(), request.currentPrice(), request.stockQuantity());
+      Product saved = productRepository.save(product);
+      return productMapper.toResponse(saved);
+      }
+      @Transactional
+    public ProductResponse.Full updatePrice(Long id, UpdatePriceRequest request) { 
+      Product product=findProductOrThrow(id);
+      product.updatePrice(request.currentPrice());
+      Product saved = productRepository.save(product);
+      return productMapper.toResponse(saved);
+     }
+     @Transactional
+    public void delete(Long id) {
+      Product product=findProductOrThrow(id);
+      productRepository.delete(product);
+    }
+    
+    //private methods 
+    private Product findProductOrThrow(Long id) {
+    return productRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Product not found: " + id));
+}
+}
